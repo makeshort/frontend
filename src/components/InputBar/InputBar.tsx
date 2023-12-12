4efcd8b875bbb1da5d58897daf4baa361574f126
jus1d <artfa63@gmail.com>
2023-12-12 12:32:45 +0400

    chore: add configProvider to inputs

--- src/components/InputBar/InputBar.tsx
+++ src/components/InputBar/InputBar.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import { Input, Select } from "antd";
+import { Input, Select, ConfigProvider } from "antd";
 import './InputBar.css';
 
 const { Option } = Select;
 
 const selectBefore = (
-    <Select defaultValue="sh.jus1d.ru">
-        <Option value="sh.jus1d.ru">sh.jus1d.ru</Option>
-        <Option value="sh.jus1d.online">sh.jus1d.online</Option>
+    <Select defaultValue="sh.jus1d.ru/">
+        <Option value="sh.jus1d.ru/">sh.jus1d.ru/</Option>
+        {/*<Option value="sh.jus1d.online/">sh.jus1d.online/</Option>*/}
     </Select>
 );
 
@@ -26,24 +26,38 @@ const InputBar: React.FC = () => {
 
     return (
         <div>
-            <label htmlFor="myInput">choose url:</label>
-            <Input
-                size="large"
-                className="typed"
-                value={inputUrl}
-                onChange={handleChangeInputUrl}
-                placeholder="example: https://my-very-long-url.tech/and-more-data"
-            />
-            <p>typed: {inputUrl}</p>
+            <ConfigProvider
+                theme={{
+                    token: {
+                        fontFamily: "Source Code Pro",
+                        borderRadiusLG: 16,
+                    },
+                    components: {
+                        Input: {
+                            activeShadow: "0 0 0 1px rgba(5, 145, 255, 1)",
+                            addonBg: "#FAFAFA",
+                        },
+                    },
+                }}>
+                <label htmlFor="myInput">choose url:</label>
+                <Input
+                    size="large"
+                    className="typed"
+                    value={inputUrl}
+                    onChange={handleChangeInputUrl}
+                    placeholder="example: https://my-very-long-url.tech/and-more-data"
+                />
+                <p>typed: {inputUrl}</p>
 
-            <Input
-                size="large"
-                addonBefore={<span className="custom-addon"> {selectBefore} </span>}
-                value={inputShortUrl}
-                onChange={handleChangeInputShortUrl}
-                placeholder="example: my-socials"
-            />
-            <p>typed: {inputShortUrl}</p>
+                <Input
+                    size="large"
+                    addonBefore={<span className="custom-addon"> {selectBefore} </span>}
+                    value={inputShortUrl}
+                    onChange={handleChangeInputShortUrl}
+                    placeholder="example: my-socials"
+                />
+                <p>typed: {inputShortUrl}</p>
+            </ConfigProvider>
         </div>
     );
 }
