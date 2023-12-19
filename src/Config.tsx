ba037baed63021ad451b495839505e9069dc12d2
jus1d <artfa63@gmail.com>
2023-12-19 15:56:56 +0400

    chore: move HomeTheme to file

---
+++ src/Config.tsx
@@ -0,0 +1,25 @@
+export const HomeTheme = {
+    token: {
+        fontFamily: "Source Code Pro",
+        borderRadiusLG: 16,
+        colorBgContainer: "#191919",
+        colorBorder: "#8FC9FE",
+        colorText: "#8FC9FE",
+        colorTextPlaceholder: "#3e5870",
+    },
+    components: {
+        Input: {
+            addonBg: "#FAFAFA",
+            activeShadow: "0px 0px 5px rgba(148, 186, 233), 0px 0px 15px rgba(148, 186, 233), 0px 0px 100px rgba(148, 186, 233)",
+            // activeShadow: "0px 0px 30px rgba(148, 186, 233)",
+            errorActiveShadow: "0px 0px 5px rgba(252, 116, 116), 0px 0px 15px rgba(252, 116, 116), 0px 0px 100px rgba(252, 116, 116)",
+            // errorActiveShadow: "0px 0px 30px rgba(252, 116, 116)",
+            colorBgBase: "#191919"
+        },
+        Button: {
+            colorBgContainerDisabled: "transparent",
+            colorTextDisabled: "#3e5870",
+            borderColorDisabled: "#3e5870"
+        }
+    },
+}
--- src/components/InputBar/InputBar.tsx
+++ src/components/InputBar/InputBar.tsx
@@ -1,6 +1,7 @@
 import { Input, ConfigProvider, Button, message } from "antd";
 import React, { useState } from "react";
 import './InputBar.css';
+import {HomeTheme} from "../../Config.tsx";
 
 interface CreatedUrl {
     id: string,
@@ -129,32 +130,7 @@ const InputBar: React.FC = () => {
     return (
         <div>
             {contextHolder}
-            <ConfigProvider
-                theme={{
-                    token: {
-                        fontFamily: "Source Code Pro",
-                        borderRadiusLG: 16,
-                        colorBgContainer: "#191919",
-                        colorBorder: "#8FC9FE",
-                        colorText: "#8FC9FE",
-                        colorTextPlaceholder: "#3e5870",
-                    },
-                    components: {
-                        Input: {
-                            addonBg: "#FAFAFA",
-                            activeShadow: "0px 0px 5px rgba(148, 186, 233), 0px 0px 15px rgba(148, 186, 233), 0px 0px 100px rgba(148, 186, 233)",
-                            // activeShadow: "0px 0px 30px rgba(148, 186, 233)",
-                            errorActiveShadow: "0px 0px 5px rgba(252, 116, 116), 0px 0px 15px rgba(252, 116, 116), 0px 0px 100px rgba(252, 116, 116)",
-                            // errorActiveShadow: "0px 0px 30px rgba(252, 116, 116)",
-                            colorBgBase: "#191919"
-                        },
-                        Button: {
-                            colorBgContainerDisabled: "transparent",
-                            colorTextDisabled: "#3e5870",
-                            borderColorDisabled: "#3e5870"
-                        }
-                    },
-                }}>
+            <ConfigProvider theme={ HomeTheme }>
                 <div className="input">
                     <div className="input-header">
                         <b>
