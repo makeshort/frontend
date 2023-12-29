738dfd959c93a46001ecb839ac5e2c0938911357
jus1d <artfa63@gmail.com>
2023-12-29 15:54:00 +0400

    chore: add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,22 @@
+import styles from "@/styles/404.module.css";
+import React from 'react';
+import Navbar from "@/components/navbar/navbar";
+
+const Custom404: React.FC = () => {
+    return (
+        <div>
+            <Navbar/>
+
+            <div className={styles.error}>
+                <div className={styles.errorCode}>
+                    404:
+                </div>
+                <div className={styles.errorText}>
+                    page not found
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Custom404;
